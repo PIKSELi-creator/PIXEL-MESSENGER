8d1dd89f1a9fe3d00508bd5b3272627d9d326a08
PIKSELi-creator <aminosav898@gmail.com>
2026-09-20 13:38:49 +0300

    Update build.gradle.kts

--- android-ui/app/build.gradle.kts
+++ android-ui/app/build.gradle.kts
@@ -1,25 +1,33 @@
 plugins {
-id("com.android.application")
-id("org.jetbrains.kotlin.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-namespace = "com.pixelchat.ui"
-compileSdk = 35
-buildToolsVersion = "36.0.0"
+    namespace = "com.pixelchat.ui"
+    compileSdk = 35
+    buildToolsVersion = "36.0.0"
 
-defaultConfig {  
-    applicationId = "com.pixelchat.ui"  
-    minSdk = 26  
-    targetSdk = 35  
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 
-    versionCode = 1  
-    versionName = "0.1.0"  
-}
+    kotlinOptions {
+        jvmTarget = "21"
+    }
 
+    defaultConfig {
+        applicationId = "com.pixelchat.ui"
+        minSdk = 26
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "0.1.0"
+    }
 }
 
 dependencies {
-implementation("androidx.core:core-ktx:1.15.0")
-implementation("androidx.appcompat:appcompat:1.7.0")
-} 
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
