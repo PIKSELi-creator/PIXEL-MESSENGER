8ddc7f2a76587d170568daa9b678f5167a4f78ae
PIKSELI-creator <aminosav898@email.com>
2026-09-25 00:48:41 +0300

    Fix Pixel Gift tile weight scope

--- android-ui/app/src/main/java/com/pixelchat/ui/MainActivity.kt
+++ android-ui/app/src/main/java/com/pixelchat/ui/MainActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -1769,7 +1768,7 @@ private fun PixelFullGiftsCard() {
 }
 
 @Composable
-private fun PixelGiftTile(symbol: String, name: String) {
+private fun RowScope.PixelGiftTile(symbol: String, name: String) {
     Column(
         modifier = Modifier
             .weight(1f)
